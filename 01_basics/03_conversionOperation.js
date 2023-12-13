c7ffa3795336d3c336f5fb20fe3b48da78355408
Shreya0507 <shreyawaghole0507@gmail.com>
2023-12-13 10:01:26 +0530

    Conversion of Datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,25 @@
+// Conversion for Numbers
+// //let score = 33 For this it's a number
+// //let score = "Shreya" For this it's a NaN
+// //let score = true For this it's a 1
+// //let score = false For this it's a 0
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// Conversion for boolean
+// let isLoggedIn = 1 //For this it's true
+// let isLoggedIn = 0 //For this it's false
+// let isLoggedIn = "" //For this it's false
+// let isLoggedIn = "Shreya" //For this it's true
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// Conversion for String
+// let someNumber = 33
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof(stringNumber));
