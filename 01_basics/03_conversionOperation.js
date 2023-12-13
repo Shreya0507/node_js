35c9dce4e66e839ed194885fb7abba72f9fabc4a
Shreya0507 <shreyawaghole0507@gmail.com>
2023-12-13 10:31:34 +0530

    Conversion of Datatypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -23,3 +23,18 @@
 // let stringNumber = String(someNumber)
 // console.log(stringNumber);
 // console.log(typeof(stringNumber));
+
+//Operations
+let value = 5
+let negValue = -value
+console.log(negValue);
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 / 2);
+// console.log(2 ** 3);
+// console.log(4 % 3);
+
+
+
