1644ad4a31e4482d70f97ea6b2d1e38b4315b9d3
Shreya0507 <shreyawaghole0507@gmail.com>
2023-12-13 10:45:49 +0530

    Comparison in js

---
+++ 01_basics/04_Comparison.js
@@ -0,0 +1,18 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log(null > 0); 
+// // Here comparison treats null as 0 so null > 0 will be false
+// console.log(null == 0);
+// // Here equality treats null as null so null == 0 will be false
+// console.log(null >= 0);
+// // Here comparison treats null as 0 so null >= 0 will be true
+
+console.log("2" > 1);
+console.log("02" < 1);
+console.log("2" == 2); //Only values check 
+console.log("2" === 2); //datatype also check
+
