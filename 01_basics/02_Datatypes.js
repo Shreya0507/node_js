7b5cd30fc79145ca5be47a13953196b13fa604e0
Shreya0507 <shreyawaghole0507@gmail.com>
2023-12-13 09:20:47 +0530

    Datatypes in Js

---
+++ 01_basics/02_Datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all Js code as newer version
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+console.log("Shreya")
+
+let name = "Shreya"
+let age = 20
+let isLoggedIn = false
+
+// number => 2 to power 53
+// BigInt
+// String => ""
+// Boolean => true/false
+// null => standalone value
+// undefined
+// Symbol
+// Object
+
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
