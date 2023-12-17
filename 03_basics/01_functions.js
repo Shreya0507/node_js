f7108207ac37ffb3b034973a3f937800cc2b1e10
Shreya0507 <shreyawaghole0507@gmail.com>
2023-12-17 22:34:04 +0530

    functions in js

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,31 @@
+// function myName(){
+//     console.log("S");
+//     console.log("H");
+//     console.log("R");
+//     console.log("E");
+//     console.log("Y");
+//     console.log("A");
+// }
+
+// function num(num1, num2){
+//     return num1 + num2;
+// }
+
+// console.log(myName(5,10));
+
+// function loginUserMessage(username){
+//     return `${username} just logged in`
+// }
+
+// console.log(loginUserMessage("Shreya"));
+
+function loginUserMessage(username){
+    // if (username === undefined) {
+    if (!username) {
+        console.log("Please enter name");
+        return
+    }
+    return `${username} just logged in`
+}
+
+console.log(loginUserMessage());
